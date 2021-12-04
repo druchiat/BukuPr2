cff947a846f9a44e26bcc2af2c8606e5a441480b
druchiat <62007520+druchiat@users.noreply.github.com>
2021-12-04 16:18:01 +0700

    [ADDD] CreateBukuPr Layout

---
+++ app/src/main/java/com/example/bukupr2/CreateBukuPrActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bukupr2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateBukuPrActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_buku_pr)
+    }
+}
